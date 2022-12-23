150674a5ad5916b952f2319144481810542f0ec6
Onimisea <onimisea@gmail.com>
2022-12-23 16:05:09 +0100

    Adding lower subcategories

--- inventory/models.py
+++ inventory/models.py
@@ -182,7 +182,7 @@ class LowerSubcategory(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
-        ordering = ["-created_at"]
+        ordering = ["created_at"]
         verbose_name = _("Lower Subcategory")
         verbose_name_plural = _("Lower Subcategories")
 
