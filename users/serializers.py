401eb4234d74da176355af01f2003dbe0e8df796
Onimisea <onimisea@gmail.com>
2022-12-29 15:27:33 +0100

    Fixing an error suspected to be caused by date input formats

--- users/serializers.py
+++ users/serializers.py
@@ -9,7 +9,7 @@ class CreateUserSerializer(serializers.ModelSerializer):
     email = serializers.CharField(max_length=60)
     phone = serializers.CharField(max_length=11)
     gender = serializers.CharField(max_length=6)
-    dob = serializers.DateField(format="%d/%m/%Y", input_formats=["%d/%m/%Y"])
+    dob = serializers.DateField(format=["%d/%m/%Y"], input_formats=["%d/%m/%Y"])
     password = serializers.CharField(write_only=True)
 
     class Meta:
