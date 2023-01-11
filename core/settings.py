a7937c1b5675c715fb0116efdeffbb0a9157df49
Onimisea <onimisea@gmail.com>
2023-01-11 11:34:17 +0100

    Have made a lot of changes

--- core/settings.py
+++ core/settings.py
@@ -134,23 +134,23 @@ WSGI_APPLICATION = "core.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.mysql",
-#         "NAME": "railway",
-#         "USER": "root",
-#         "PASSWORD": "ErqMFdplwCMaCFldedAJ",
-#         "HOST": "containers-us-west-111.railway.app",
-#         "PORT": "6664",
-#     }
-# }
-
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "railway",
+        "USER": "root",
+        "PASSWORD": "ErqMFdplwCMaCFldedAJ",
+        "HOST": "containers-us-west-111.railway.app",
+        "PORT": "6664",
     }
 }
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 
 
 # Password validation
